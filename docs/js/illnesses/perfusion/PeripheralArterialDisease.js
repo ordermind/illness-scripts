451fdde6d1681cccc422b888dcf408dadd115981
Kristofer Tengstrom <info@kristofer-tengstrom.se>
2024-03-15 17:04:29 +0100

    Add page for peripheral arterial disease

--- docs/js/illnesses/perfusion/PeripheralArterialDisease.js
+++ docs/js/illnesses/perfusion/PeripheralArterialDisease.js
@@ -14,6 +14,16 @@ Ondercategorieën:
 - Chronisch obstructief arterieel vaatlijden: geleidelijk progressieve perfusiestoornis van een voet of onderbeen, met 2 uitingsvormen:
     - claudicatio intermittens
     - kritieke ischemie
+
+Comorbiditeiten:
+
+- diabetes mellitus
+- chronisch obstructieve longziekten (COPD)
+- hartfalen
+- angina pectoris
+- cerebrovasculair accident (CVA)
+- artrose
+- klachten van wervelkolom/SI-gewricht/heupgewricht/kniegewricht, voet(en)
     `,
     epidemiology: `
 #### Acute ischemie
@@ -132,25 +142,146 @@ Let bij de beoordeling op links-rechtsverschillen.
 - Gevoelsstoornissen van het been (vaak de interdigitale ruimte of de voetrug tussen 1e en 2e straal)
 - Spierzwakte van het been (vaak zwakte of motorische uitval van de intrinsieke voetspieren)
 
-##### Chronisch obstructief arterieel vaatlijden
+##### Claudicatio intermittens
 
-- Verzwakte arteriële pulsaties bij palpatie van a. tibialis posterior, a. dorsalis pedis en/of a. femoralis
-- Souffle bij auscultatie van de a. femoralis
-- Lagere huidtemperatuur van voeten en onderbenen bij palpatie met de handrug
-- Trofische stoornissen: beoordeel de huid (vooral van de tenen, met aandacht voor wondjes), nagels, beharing van voeten en onderbenen
-- Bepaal de enkel-armindex (EAI) bij klachten van claudicatio intermittens en/of als een van de bovenstaande onderzoeken een positief resultaat geeft. Een enkel-armindex in rust < 0,9 ondersteunt de diagnose ‘chronisch obstructief arterieel
-vaatlijden.
+<table class="table table-striped">
+    <tr><td>
+Verzwakte arteriële pulsaties bij palpatie van a. tibialis posterior, a. dorsalis pedis en/of a. femoralis
+    </td></tr>
+    <tr><td>
+Souffle bij auscultatie van de a. femoralis
+    </td></tr>
+    <tr><td>
+Lagere huidtemperatuur van voeten en onderbenen bij palpatie met de handrug
+    </td></tr>
+    <tr><td>
+Trofische stoornissen: beoordeel de huid (vooral van de tenen, met aandacht voor wondjes), nagels, beharing van voeten en onderbenen
+    </td></tr>
+    <tr><td>
+Bepaal de enkel-armindex (EAI) bij klachten van claudicatio intermittens en/of als een van de bovenstaande onderzoeken een positief resultaat geeft. Een enkel-armindex in rust
 
-###### Kritieke ischemie
+Interpretatie van EAI onderzoek:
+
+< 0,8 (eenmalige bepaling) of < 0,9 (gemiddelde van 3 bepalingen): chronisch obstructief arterieel vaatlijden vrijwel zeker (kans > 95%)
+
+0,9-1,0: chronisch obstructief arterieel vaatlijden niet zeker, overweeg alternatieve diagnosen of verwijs voor nadere diagnostiek bij een aanhoudend vermoeden van chronisch obstructief arterieel vaatlijden
+
+> 1,1 (eenmalige bepaling) of > 1,0 (gemiddelde van 3 bepalingen): chronisch obstructief
+arterieel vaatlijden vrijwel uitgesloten (kans < 1%)
+    </td></tr>
+    <tr><td>
+Evalueren van het looppatroon
+    </td></tr>
+    <tr><td>
+Loopbandtest:
+
+- loopsnelheid 3,2 km/uur
+- toename hellingshoek met 2% elke twee minuten beginnend bij 0%, tot maximaal 10%
+- duur van de test maximaal 30 minuten
+
+Na afname van de loopbandtest wordt de loopafstand vastgelegd. Tevens wordt gelet op signalen die de loopafstand van de patiënt kunnen beperken:
+
+- lokalisatie van de pijn
+- ernst van de pijn (gebruik hiervoor de <a target="_blank" href="https://meetinstrumentenzorg.nl/instrumenten/american-college-of-sport-medicin-schaal/">ACSM schaal voor pijn</a>)
+- uitputting
+- pijn op de borst
+- benauwdheid
+- angst
+
+    </td></tr>
+    <tr><td>
+Aanvullend functieonderzoek:
+
+- vaststellen van de spierkracht
+- mobiliteitsonderzoek, b.v. <a target="_blank" href="https://meetinstrumentenzorg.nl/instrumenten/walking-impairment-questionnaire/">Walking Impairment Questionnaire (WIQ)</a>
+- evaluatie van coördinatie/stabiliteit
+- sensibiliteitsonderzoek (in geval van diabetes mellitus)
+    </td></tr>
+</table>
+
+##### Kritieke ischemie
 
 - Systolische enkeldruk is < 50 mmHg.
 
+#### Differentiaaldiagnostiek
+
+<strong>Lumbale wervelkanaalstenose (neurogene claudicatio)</strong>, ook <strong>spinale stenose</strong> genoemd, is een vernauwing van het wervelkanaal waardoor zenuwen geklemd worden, en dat kan klachten veroorzaken tijdens het lopen zoals pijn in rug en/of benen, tintelingen en spierzwakte. In tegenstelling tot PAV heb je hierbij geen verminderde cirkulatie en dus ook geen kleurafwijkingen,  temperatuurverschillen of andere gerelateerde verschijnsels.
+
+<strong>Perifere diabetische neuropathie</strong> treedt op wanneer de sensorische, motorische en autonome zenuwen van het perifere zenuwstelsel bij mensen met diabetes niet goed functioneren. Sommige patiënten ervaren pijnlijke sensorische neuropathie, waaronder een brandend of tintelend gevoel dat bekendstaat als paresthesie. In tegenstelling tot PAV heb je hierbij geen verminderde cirkulatie en dus ook geen kleurafwijkingen, temperatuurverschillen of andere gerelateerde verschijnsels.
+
+<strong>Artrose</strong> veroorzaakt pijn in gewrichten terwijl bij PAV de pijn is niet beperkt tot gewrichten. Bij artrose heb je ook geen verminderde cirkulatie en dus ook geen kleurafwijkingen, temperatuurverschillen of andere gerelateerde verschijnsels.
+
+<strong>Chronische veneuze insufficiëntie (CVI)</strong> betekent dat de venen niet meer goed functioneren, bijvooreeld vanwege <strong>varices (spataderen)</strong>. Meestal ontstaat een moe, loom en zwaar gevoel in de benen. Ook kan een vochtophoping rond de enkels ontstaan. Net als bij PAV kunnen klachten ontstaan tijdens lopen, maar de bloeddruk in de enkel zal hierbij niet lager zijn, dus een positieve EAI zou dit moeten kunnen uitsluiten.
+
+Bij <strong>chronisch compartimentsyndroom</strong> ontstaat door er een te hoge druk in de spieren, na of tijdens sporten of wandelen, pijn en krampen in het onderbeen. Meestal zijn de klachten bilateraal terwijl PAV vaak unilateraal is. Je hebt hierbij ook geen trofische stoornissen zoals broze nagels of een dunne glanzende huid bij de voet.
+
+Een <strong>Bakercyste</strong> veroorzaakt zwelling en pijn achter de knie, maar deze symptomen zijn niet specifiek gerelateerd aan inspanning en verbeteren niet met rust zoals bij PAV.
     `,
     treatment: `
+#### Acute ischemie
 
+<strong>Bij vermoeden op acute ischemie, verwijs met spoed naar een vaatchirurg (of bel 112)</strong>
+
+- Trombolyse (oplossen van stolsel door een infuus van een krachtige bloedverdunner)
+- Trombo-embolectomie (operatief verwijderen van een stolsel uit het bloedvat)
+
+#### Claudicatio intermittens
+
+- Educatie - geven van informatie en voorlichting ter bevordering van het ziekte-inzicht van de patiënt
+- Cardiovasculaire risicomanagement
+    - Stoppen met roken
+- Verbeteren van het ( subjectieve en objectieve) inspanningsvermogen
+    - het vergroten van de functionele loopafstand
+    - het vergroten van het aeroob uithoudingsvermogen
+    - het leren omgaan met de klachten door:
+        - het verhogen van de pijntolerantie
+        - het overwinnen van angst voor inspanning
+- Optimaliseren van activiteiten en participatie van de patiënt
+
+Looptraining - minimaal 30 minuten per sessie, afbouwend naar steeds meer zelfstandig
+oefenen
+
+Voorbeeld van een behandelschema:
+<table class="table">
+    <tr>
+        <th>Fase</th>
+        <th>Behandelfrequentie</th>
+        <th>Duur fase</th>
+        <th>Aantal behandelingen</th>
+    </tr>
+    <tr>
+        <td>Startfase</td>
+        <td>3 maal per week</td>
+        <td>4 weken</td>
+        <td>12</td>
+    </tr>
+    <tr>
+        <td>Zelf-/trainingsfase</td>
+        <td>1-2 maal per week</td>
+        <td>8 weken</td>
+        <td>8-16</td>
+    </tr>
+    <tr>
+        <td>Onderhoudsfase</td>
+        <td>1-2 maal per maand</td>
+        <td>40 weken</td>
+        <td>9-18</td>
+    </tr>
+</table>
+
+#### Kritieke ischemie
+
+<strong>Bij vermoeden op kritieke ischemie, verwijs met spoed naar een vaatchirurg (of bel 112)</strong>
+
+- Percutane Transluminale Angioplastiek (PTA, "dotteren")
+- Bypass
+- Endarteriëctormie
+- Amputatie
     `,
     references: `
 Bartelink, M.E.L., Elsman, B.H.P., Oostindjer, A., Stoffers, H.E.J.H., Wiersma, T.J., Geraets, J.J.X.R. (2014). <em>NHG-Standaard
 Perifeer arterieel vaatlijden (M13)</em>. Nederlands Huisartsen Genootschap. Geraadpleegd op 15 maart 2024, van https://richtlijnen.nhg.org/files/pdf/79_Perifeer%20arterieel%20vaatlijden_februari-2014.pdf
+
+Merry, A.H.H., Teijink, J.A.W., Jongert, M.W.A., Poelgeest, A., van der Voort, S.S.E.M., Bartelink, M.E.L., Stoffers, H.E.J.H., Brooymans, F., Hendriks, H.J.M., de Bie, R.A. (2014). <em>KNGF-richtlijn Symptomatisch perifeer arterieel vaatlijden</em>. Koninklijk Nederlands Genootschap voor Fysiotherapie (KNGF). Geraadpleegd op 15 maart 2024, van https://www.kngf.nl/binaries/content/assets/kennisplatform/onbeveiligd/richtlijnen/spav/downloads/spav-praktijkrichtlijn
     `
 });
