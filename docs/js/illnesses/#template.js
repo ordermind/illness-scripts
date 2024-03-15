738d9cb4504f837ede16a69655bee4aee7bdc0e7
Kristofer Tengström <info@kristofer-tengstrom.se>
2024-03-15 10:10:03 +0100

    Add illness template

---
+++ docs/js/illnesses/#template.js
@@ -0,0 +1,28 @@
+import Illness from "../Illness.js";
+
+export default new Illness({
+    id: "",
+    name: "",
+    images: [
+        "./images/",
+    ],
+    pathophysiology: `
+
+    `,
+    epidemiology: `
+
+    `,
+    timeCourse: "",
+    symptoms: `
+
+    `,
+    diagnostics: `
+
+    `,
+    treatment: `
+
+    `,
+    references: `
+
+    `
+});
